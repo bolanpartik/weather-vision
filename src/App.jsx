f28d3f4dee247b2003b9626fceecde2c5ec742e6
Partik Bolan <bolanpartik@gmail.com>
2024-09-25 13:06:39 +0530

    import NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react"
+import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
-    <>
+
+      <div>
+        <NavBar />
+      </div>
       
-    </>
   )
 }
 
