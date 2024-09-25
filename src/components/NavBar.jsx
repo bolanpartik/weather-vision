1bbdfdb34415a71939989452308b5e9e651ebabc
Partik Bolan <bolanpartik@gmail.com>
2024-09-25 09:53:50 +0530

    NavBar created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+export default function NavBar() {
+    return (
+        <div>
+            <div>
+                <img src='' alt="logo here"/>
+                <p>Weather Vision</p>
+            </div>
+            <div>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Support</a>
+                <a href="">Contact</a>
+            </div>
+        </div>
+    )
+}
