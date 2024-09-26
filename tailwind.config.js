821295529455b03af3b3c16bd2c38b37fa43e9f5
Partik Bolan <bolanpartik@gmail.com>
2024-09-26 09:03:48 +0530

    add custom backgrounds, color and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customDark: '#050520',
+      },
+      backgroundImage: {
+        darkGradient: 'linear-gradient(135deg, rgb(0, 0, 0), rgb(9, 9, 83), rgb(0, 0, 0), rgb(0, 0, 15), rgb(9, 9, 83), rgb(0, 0, 0), rgb(0, 0, 0))',
+        bgButton    : 'linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)'
+      },
+      backgroundSize:{
+        buttonSize : '200%,100%',
+      },
+      animation: {
+        shimmer: "shimmer 3s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
-
