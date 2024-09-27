31e328ab64ab27c6a9d1d7469aa886a652dc830d
Partik Bolan <bolanpartik@gmail.com>
2024-09-27 19:13:27 +0530

    styles added for GetInfo

--- src/components/GetInfo.jsx
+++ src/components/GetInfo.jsx
@@ -1,9 +1,9 @@
 export default function GetInfo({ searchCity, setSearchCity }) {
 
     return (
-        <div>
-            <p>Enter City</p>
-            <div>
+        <div className="w-full flex flex-col items-center p-12 mt-4 font-serif">
+            <p className="text-4xl w-full text-center">Enter City</p>
+            <div className="flex gap-4 w-full justify-center mt-3">
                 <input
                     type="text"
                     placeholder="enter city"
@@ -11,8 +11,10 @@ export default function GetInfo({ searchCity, setSearchCity }) {
                     onChange={(e) => {
                         setSearchCity(e.target.value);
                     }}
+                    className="input w-full max-w-xs placeholder:opacity-60"
                 />
-                <button>
+                <button
+                    className="h-12 animate-shimmer rounded-md border border-slate-800 bg-bgButton bg-buttonSize px-6 text-slate-200 transition-colors hover:border-slate-500 ">
                     Get Info
                 </button>
             </div>
