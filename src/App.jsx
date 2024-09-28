4112ea6cb4d3a56a11910580ae1bd4e93c0c54bd
Partik Bolan <bolanpartik@gmail.com>
2024-09-28 12:43:55 +0530

    import HeroComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 import NavBar from "./components/NavBar"
+import HeroSection from "./components/HeroSection"
 
 function App() {
 
   return (
 
-      <div>
-        <NavBar />
-      </div>
-      
+    <div>
+      <NavBar />
+      <HeroSection />
+    </div>
   )
 }
 
