db2e0a389ff71d6896912adcb5cecbbe69e8a633
Partik Bolan <bolanpartik@gmail.com>
2024-09-28 10:36:10 +0530

    fetch coordinates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,9 +4,23 @@ import GetInfo from "./GetInfo";
 export default function HeroSection() {
     const [searchCity, setSearchCity] = useState('')
     const [loading, setLoading] = useState(false)
-    
-    const getLatAndLon = () => {
-        // code to fetch the coordinates of city
+    const [coordinates, setCoordinates] = useState([])
+
+    const apiKey = import.meta.env.VITE_API_KEY
+
+    const getLatAndLon = async () => {
+        try {
+            setLoading(true)
+            const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${apiKey}`)
+            const parsedRes = await response.json()
+            const newCoordinates = parsedRes.map(location => ({ lat: location.lat, lon: location.lon }))
+            setCoordinates(newCoordinates)
+        } catch (e) {
+            console.log('Error: ', e)
+            setLoading(false)
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
