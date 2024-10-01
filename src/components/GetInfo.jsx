320dae7eddc8df231690e2e03106bf9237c7bc91
Partik Bolan <bolanpartik@gmail.com>
2024-10-01 22:16:38 +0530

    add media query and fix layout

--- src/components/GetInfo.jsx
+++ src/components/GetInfo.jsx
@@ -4,10 +4,10 @@ export default function GetInfo({ searchCity, setSearchCity, getLatAndLon, loadi
 
     return (
         <div className="w-full flex flex-col items-center mt-8 font-serif">
-            <div className="bg-customDark/20 p-10 rounded-xl w-1/2" >
+            <div className="bg-customDark/20 p-5 md:p-10 rounded-xl w-full md:w-1/2" >
 
-                <p className="text-4xl w-full text-center">Enter City</p>
-                <div className="flex gap-4 w-full justify-center mt-3">
+                <p className="text-2xl md:text-4xl w-full text-center">Enter City</p>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full shrink justify-center mt-3">
                     <input
                         type="text"
                         placeholder="enter city"
@@ -20,8 +20,8 @@ export default function GetInfo({ searchCity, setSearchCity, getLatAndLon, loadi
                     <button
                         onClick={getLatAndLon}
                         disabled={searchCity === ''}
-                        className="h-12 animate-shimmer rounded-md border border-slate-800 bg-bgButton bg-buttonSize px-6 text-slate-200 transition-colors hover:border-slate-500 disabled:cursor-not-allowed">
-                        {loading ? <Loader2 className="animate-spin" /> : ' Get Info '}
+                        className="h-12 text-sm md:text-base animate-shimmer rounded-md border border-slate-800 bg-bgButton bg-buttonSize px-6 text-slate-200 transition-colors hover:border-slate-500 disabled:cursor-not-allowed max-w-xs shrink-0">
+                        {loading ? <Loader2 className="animate-spin" /> : 'Get Info'}
                     </button>
                 </div>
             </div>
