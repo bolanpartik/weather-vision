79485834c2b4639a59bfc34c939a25a50f57c3ce
Partik Bolan <bolanpartik@gmail.com>
2024-10-01 22:49:26 +0530

    improve layout and responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import Logo from '../assets/logo.jpeg'
 
 export default function NavBar() {
     return (
-        <div className='flex justify-between w-2/3 border border-zinc-400/50 rounded-full items-center p-2 shadow-lg shadow-emerald-100/20 text-fuchsia-600 backdrop-blur-md'>
+        <div className='flex justify-between w-full md:w-2/3 rounded-full items-center p-2 text-fuchsia-600 '>
 
             <div className='flex items-center'>
                 <img
@@ -10,10 +10,10 @@ export default function NavBar() {
                     alt="logo here"
                     className='h-14 rounded-full'
                 />
-                <p className='ml-3 text-3xl font-semibold text-green-500'>Weather Vision</p>
+                <p className='ml-3 hidden md:inline text-xl sm:text-2xl font-semibold text-green-500'>Weather Vision</p>
             </div>
 
-            <div className='flex gap-4 mr-3 transition-all duration-200'>
+            <div className='flex gap-2 md:gap-4 mr-3 transition-all duration-200'>
                 <a href="" className='hover:text-indigo-100'>Home</a>
                 <a href="" className='hover:text-indigo-100'>About</a>
                 <a href="" className='hover:text-indigo-100'>Support</a>
